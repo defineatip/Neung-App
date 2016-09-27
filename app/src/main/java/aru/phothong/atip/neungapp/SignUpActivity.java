c9db4a6827c453ea5fad1d9c65081ce30ec7c729
neung <p_atip99@hotmail.com>
2016-09-27 15:28:08 +0700

    Know Choose Image

--- app/src/main/java/aru/phothong/atip/neungapp/SignUpActivity.java
+++ app/src/main/java/aru/phothong/atip/neungapp/SignUpActivity.java
@@ -1,5 +1,6 @@
 package aru.phothong.atip.neungapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,8 +26,34 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.editText4);
         imageView = (ImageView) findViewById(R.id.imageView);
 
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกรูปภาพ"), 0);
+
+
+            }//onClick
+        });
+
 
     }//Main Method
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode==0)&&(resultCode==RESULT_OK)) {
+
+            aBoolean = false;
+
+        }//if
+
+    }//onActivityResult
 
     public void clicksignUpsign(View view) {
         //get Value From Edit Text
